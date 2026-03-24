a8c2c75d843fa4854f6e14ef069959440a6b294e
khmaher <katymaheris@gmail.com>
2026-03-24 13:41:52 +0000

    Add in confidence threshold option

--- scripts/06_kraken.sh
+++ scripts/06_kraken.sh
@@ -14,27 +14,29 @@ conda activate shotgun_meta
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -k parameterK -d parameterD -f parameterF -r parameterR"
+   echo "Usage: $0 -k parameterK -d parameterD -f parameterF -r parameterR -c parameterC"
    echo -e "\t-k the location of your Kraken database"
    echo -e "\t-d the directory containing your input data files"
    echo -e "\t-f the file extension for your forward reads"
    echo -e "\t-r the file extension for your reverse reads"
+   echo -e "\t-c the confidence score threshold"
    exit 1 # Exit script after printing help
 }
 
-while getopts "k:d:f:r:" opt
+while getopts "k:d:f:r:c:" opt
 do
    case "$opt" in
    	  k ) parameterK="$OPTARG" ;;
       d ) parameterD="$OPTARG" ;;
 	  f ) parameterF="$OPTARG" ;;
       r ) parameterR="$OPTARG" ;;
+	  c ) parameterR="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterK" ] || [ -z "$parameterD" ] || [ -z "$parameterF" ] || [ -z "$parameterR" ]
+if [ -z "$parameterK" ] || [ -z "$parameterD" ] || [ -z "$parameterF" ] || [ -z "$parameterR" ] || [ -z "$parameterC" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -51,6 +53,6 @@ mkdir $src/kraken2
 for f in $src/$parameterD/*$parameterF;
 do FBASE=$(basename $f)
 	BASE=${FBASE%$parameterF} 
-	kraken2 --threads 20 --paired --db $parameterK --output $src/kraken2/${BASE}.kraken --report $src/kraken2/${BASE}.kreport2 $src/$parameterD/${BASE}$parameterF $src/$parameterD/${BASE}$parameterR
+	kraken2 --threads 20 --paired --db $parameterK --confidence $parameterC --output $src/kraken2/${BASE}.kraken --report $src/kraken2/${BASE}.kreport2 $src/$parameterD/${BASE}$parameterF $src/$parameterD/${BASE}$parameterR
 done
 
