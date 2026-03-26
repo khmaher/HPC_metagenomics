6fd3812a5688f24ae48426fdf2c5c72d056756ad
khmaher <katymaheris@gmail.com>
2026-03-26 15:52:26 +0000

    Add files via upload

--- scripts/09_humann.sh
+++ scripts/09_humann.sh
@@ -3,13 +3,40 @@
 #SBATCH --job-name=09_humann
 #SBATCH --output=09_humann.log
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=20
 #SBATCH --mem-per-cpu=4GB
-#SBATCH --time=168:00:00
+#SBATCH --time=96:00:00
 
 source ~/.bashrc
 
 conda activate biobakery3.9
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -d parameterD -f parameterF -r parameterR"
+   echo -e "\t-d the directory containing your input data files"
+   echo -e "\t-f the file extension for your forward reads"
+   echo -e "\t-r the file extension for your reverse reads"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "d:f:r:" opt
+do
+   case "$opt" in
+      d ) parameterD="$OPTARG" ;;
+	  f ) parameterF="$OPTARG" ;;
+      r ) parameterR="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterD" ] || [ -z "$parameterF" ] || [ -z "$parameterR" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
 
 
 src=$PWD
@@ -21,20 +48,20 @@ mkdir $src/humann
 
 # cat files
 
-for f in $src/deacon/*_hostDepleted_R1.fq.gz;
+for f in $src/$parameterD/*$parameterF;
 do              FBASE=$(basename $f)
-        BASE=${FBASE%_hostDepleted_R1.fq.gz}
-        cat $src/deacon/${BASE}_hostDepleted_R1.fq.gz \
-	$src/deacon/${BASE}_hostDepleted_R2.fq.gz > \
-	$src/cat_files/${BASE}_hostDepleted_paired.fastq.gz
+        BASE=${FBASE%$parameterF}
+        cat $src/$parameterD/${BASE}$parameterF \
+	$src/$parameterD/${BASE}$parameterR > \
+	$src/cat_files/${BASE}_paired.fastq.gz
 done
 
 # run humann
 
-for f in $src/cat_files/*_hostDepleted_paired.fastq.gz;
+for f in $src/cat_files/*_paired.fastq.gz;
 do              FBASE=$(basename $f)
-        BASE=${FBASE%_hostDepleted_paired.fastq.gz}
-        humann --input $src/cat_files/${BASE}_hostDepleted_paired.fastq.gz \
+        BASE=${FBASE%_paired.fastq.gz}
+        humann --input $src/cat_files/${BASE}_paired.fastq.gz \
 	--output $src/humann/${BASE}_humann_output \
-	--threads 12 
+	--threads 20 
 done
