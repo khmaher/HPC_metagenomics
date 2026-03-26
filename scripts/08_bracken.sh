8cecd9260748faf9ae7856cf53239ef6bc4a3361
khmaher <katymaheris@gmail.com>
2026-03-26 16:34:50 +0000

    Update 08_bracken.sh

--- scripts/08_bracken.sh
+++ scripts/08_bracken.sh
@@ -18,24 +18,22 @@ helpFunction()
    echo -e "\t-r the ideal length of the reads that were used in the Kraken2 classification"
    echo -e "\t-l the taxonomic level/rank of the Bracken output"
    echo -e "\t-t the minimum number of reads required for a classification at the specified rank"
-   echo -e "\t-s The number S is chosen as 3 (first three info columns) + X*2 (X samples with 2 columns each) = S"
    exit 1 # Exit script after printing help
 }
 
-while getopts "d:r:l:t:s:" opt
+while getopts "d:r:l:t:" opt
 do
    case "$opt" in
       d ) parameterD="$OPTARG" ;;
       r ) parameterR="$OPTARG" ;;
       l ) parameterL="$OPTARG" ;;
       t ) parameterT="$OPTARG" ;;
-      s ) parameterS="$OPTARG" ;;
       #? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterD" ] || [ -z "$parameterR" ] || [ -z "$parameterL" ] || [ -z "$parameterT" ] || [ -z "$parameterS" ]
+if [ -z "$parameterD" ] || [ -z "$parameterR" ] || [ -z "$parameterL" ] || [ -z "$parameterT" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
@@ -62,11 +60,10 @@ mkdir $src/bracken_all
 
 combine_bracken_outputs.py --files $src/bracken/*.bracken -o $src/bracken_all/all.bracken
 
-# Extract Bracken output
-#Create variable
-# Final number in the seq must be 3 + 2*number of samples you have
+# Convert Bracken output to biom
 
-bracken_num_columns=$(seq -s , 4 2 $parameterS)
-echo $bracken_num_columns
+kraken-biom $src/bracken/*breport2 -o $src/bracken/All_samples.biom
 
-cat $src/bracken_all/all.bracken | cut -f 1,$bracken_num_columns > $src/bracken_all/all_num.bracken
+# Create tsv (tab separated value) file for manual inspection.
+
+biom convert -i $src/bracken/All_samples.biom -o $src/bracken/All_samples.tsv --header-key taxonomy --to-tsv
